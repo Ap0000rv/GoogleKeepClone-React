1d509fa2a010cb8c0e0ac8f93c1ca429ce06b7fc
Ap0000rv <123739980+Ap0000rv@users.noreply.github.com>
2023-02-06 15:58:59 +0530

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+	return (
+		<div className="note">
+			<h1>This is Heading of TODO</h1>
+			<p>this is Content of this TODO</p>
+		</div>
+	);
+}
+
+export default Note;
